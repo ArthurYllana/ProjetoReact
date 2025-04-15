ecca19d1cf6a7e73ecb67ac07321a50cede9c168
Arthur Yabuchi Yllana <arthur.yllana@gmail.com>
2025-04-15 11:21:43 -0300

    Add files via upload

---
+++ src/Letreiro.js
@@ -0,0 +1,13 @@
+import './Letreiro.css';
+
+function App() {
+  return (
+    <>
+         <h1>Letreiro</h1>
+         <p>Meu Letreiro</p>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/Relogio.js
@@ -0,0 +1,13 @@
+import './Relogio.css';
+
+function App() {
+  return (
+    <>
+        <h1>Relogio</h1>
+        <p>Meu relógio</p>
+    </>
+   
+  );
+}
+
+export default App;
