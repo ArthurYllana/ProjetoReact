1f6974a967334bcc1e4037294938c2c8266bfb97
ArthurYllana <arthur.yllana@gmail.com>
2025-05-20 11:55:03 -0300

    Atividade 04, falta algumas coisas

---
+++ src/pages/Atv04.js
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+export default function Atv04() {
+  return (
+    <>
+        <h1>Atividade 04</h1>
+        <Link to ="/"> Voltar </Link>
+    </>
+  );
+}
